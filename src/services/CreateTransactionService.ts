e0489462069164579b6c4529558f6afae91e198c
thgendry <thisanji@gmail.com>
2020-09-07 04:27:00 -0300

    Desafio 06 finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,47 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string,
+  value: number,
+  type: 'income' | 'outcome',
+  category: string,
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category: categoryTitle }: Request): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository)
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance.');
+    }
+
+    let category = await categoriesRepository.findOne({
+      where: { title: categoryTitle }
+    })
+    if (!category) {
+      category = categoriesRepository.create({
+        title: categoryTitle,
+      })
+      await categoriesRepository.save(category)
+    }
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: category
+    })
+
+    await transactionsRepository.save(transaction);
+    return transaction;
   }
 }
 
